fdeafcc172af72f4da2e6b52fa566b283589dc11
MurlockBot <lucasgottifredifhotmail.com>
2023-04-08 17:19:38 -0300

    Buscar categoría por ID dentro del controlador.

--- src/main/java/com/company/inventory/controllers/CategoryRestController.java
+++ src/main/java/com/company/inventory/controllers/CategoryRestController.java
@@ -2,10 +2,12 @@ package com.company.inventory.controllers;
 
 import com.company.inventory.dao.ICategoryDAO;
 import com.company.inventory.response.CategoryResponseRest;
+import com.company.inventory.response.ResponseRest;
 import com.company.inventory.services.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,12 @@ public class CategoryRestController {
     public ResponseEntity<CategoryResponseRest> searchCategories(){
 
         ResponseEntity<CategoryResponseRest> response = service.search();
+        return response;
+    }
+
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> searchById(@PathVariable Long id){
+        ResponseEntity<CategoryResponseRest> response = service.searchById(id);
         return response;
     }
 }
--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements ICategoryService{
@@ -20,12 +22,43 @@ public class CategoryServiceImpl implements ICategoryService{
     @Override
     @Transactional(readOnly = true)
     public ResponseEntity<CategoryResponseRest> search() {
+
         CategoryResponseRest response = new CategoryResponseRest();
 
         try{
             List<Category> categories = (List<Category>) categoryDAO.findAll();
 
             response.getCategoryResponse().setCategory(categories);
+            response.setMetadata("OK", "0", "Respuesta exitosa");
+        } catch (Exception e) {
+
+            response.setMetadata("NO", "-1","Error al consultar servicio en la base de datos");
+            e.getStackTrace();
+            return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+
+        CategoryResponseRest response = new CategoryResponseRest();
+        List<Category> lista =  new ArrayList<>();
+
+        try{
+            Optional<Category> category = categoryDAO.findById(id);
+
+            if(category.isPresent()){
+                lista.add(category.get());
+                response.getCategoryResponse().setCategory(lista);
+                response.setMetadata("OK", "0", "Respuesta exitosa");
+            } else{
+                response.setMetadata("NO", "-1","No se encontró la categoría con el ID suministrado.");
+                return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+            }
+
             response.setMetadata("OK", "0", "Respuesta exitosa");
         } catch (Exception e) {
 
